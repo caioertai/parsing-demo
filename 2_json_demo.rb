083cfd49405a2e001ab3999bb4f8d06c8facd338
Caio Andrade <caioertai@gmail.com>
2019-10-22 00:32:34 -0300

    Add appearance line to csv

--- 2_json_demo.rb
+++ 2_json_demo.rb
@@ -1,6 +1,8 @@
 # ============== Reading (Parsing) JSON =================
 # TODO: let's read data from beers.json
 
+# TODO: Display appearance of second beer
+
 # TODO: List the origins of each beer
 
 # TODO: List unique origins
